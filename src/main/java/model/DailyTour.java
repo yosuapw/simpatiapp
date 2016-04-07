64fd30e006b701d776520e3c4298efddbda327b2
yosua161 <yosua161@gmail.com>
2016-04-07 23:20:17 +0800

    update route

--- src/main/java/model/DailyTour.java
+++ src/main/java/model/DailyTour.java
@@ -1,11 +1,15 @@
 package model;
 
+import java.util.List;
+
 import org.mongodb.morphia.annotations.Entity;
 
 @Entity("dailyTour")
 public class DailyTour {
     
     private Price prices;
+
+	private List<Destination> destinations;
 
     public Price getPrices() {
         return prices;
@@ -14,6 +18,13 @@ public class DailyTour {
     public void setPrices(Price prices) {
         this.prices = prices;
     }
-    
+
+	public List<Destination> getDestinations() {
+		return destinations;
+	}
+
+	public void setDestinations(List<Destination> destinations) {
+		this.destinations = destinations;
+	}
     
 }
---
+++ src/main/java/model/Destination.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Destination {
+
+	private Integer index;
+	private String title;
+	private String description;
+
+	public Integer getIndex() {
+		return index;
+	}
+
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
